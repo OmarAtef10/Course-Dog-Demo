24da55037569acedcea9807b887a04ca2eae63cf
OmarAlHaj24 <omaralhaj24@gmail.com>
2023-04-13 03:01:19 +0200

    Announcement CRUD operations

--- announcement/urls.py
+++ announcement/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 
 from django.urls import path, include
+from . import views
 from .views import *
 
 urlpatterns = [
     path(r'', AnnouncementViewSet.as_view({'get': 'list', "post": "create"})),
     path('create/', add_announcement, name='announcement-create'),
+    path('<int:course_id>/', views.UploadCourseAnnouncementAPIView.as_view())
 ]

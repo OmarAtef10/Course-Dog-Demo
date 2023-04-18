592f3a90a72529ca066d7275d4ef2cab1cf215dd
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-04-18 14:15:08 +0200

    profiles webhooks logic

--- ServerDemo/urls.py
+++ ServerDemo/urls.py
@@ -19,13 +19,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('auth/', include('authentication.urls')),
-    path('courses/', include('course.urls')),
-    path('materials/', include('material.urls')),
-    path('announcements/', include('announcement.urls')),
-    path('organization/', include('organization.urls')),
+                  path('admin/', admin.site.urls),
+                  path('api-auth/', include('rest_framework.urls')),
+                  path('auth/', include('authentication.urls')),
+                  path('courses/', include('course.urls')),
+                  path('materials/', include('material.urls')),
+                  path('announcements/', include('announcement.urls')),
+                  path('organization/', include('organization.urls')),
+                  path('user-profile/', include('user_profile.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
+from .serializers import ProfileSerializer
+
 
 # Create your views here.
+def get_user_by_fb_name(request, fb_name):
+    profile = get_object_or_404(Profile, facebook_username=fb_name)
+    serializer = ProfileSerializer(profile)
+    return JsonResponse(serializer.data, safe=False)
+
+
+def get_user_by_phone_number(request, phone_number):
+    profile = get_object_or_404(Profile, whatsapp_number=phone_number)
+    serializer = ProfileSerializer(profile)
+    return JsonResponse(serializer.data, safe=False)

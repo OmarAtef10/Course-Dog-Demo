bb83f26943903adc4ac36dd89b5ef201dee6107d
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-05-23 22:15:30 +0200

    File Download with celery is now complete

--- ServerDemo/celery.py
+++ ServerDemo/celery.py
@@ -3,10 +3,12 @@
 import os
 from celery import Celery
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ServerDemo.settings")
 app = Celery("ServerDemo")
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
 
 @app.task(bind=True)
 def debug_task(self):
--- material/tasks.py
+++ material/tasks.py
@@ -9,28 +9,39 @@ from user_profile.views import creds_refresher
 
 @shared_task
 def download_materials(materials, token, course_name, user):
+    print("We are inside download materials")
     user = User.objects.get(id=user)
     token = creds_refresher(user)
     course_name = course.models.Course.objects.get(name=course_name)
+    # print(materials['courseWorkMaterial'][0]['materials'])
     for key, val in materials.items():
         for entry in val:
+            print("ENTRY :- ", entry)
+            print("ENTRY ID :- ", entry['id'])
             print("Finding material")
-            material = Material.objects.filter(id=entry['id'])
-            if not material:
-                try:
-                    print("HERE!!")
-                    material = Material(id=entry['materials'][0]['driveFile']['driveFile']['id'],
-                                        parent_course=course_name,
-                                        title=entry['title'],
-                                        file_name=entry['materials'][0]['driveFile']['driveFile']['title'],
-                                        creation_date=entry['creationTime'])
-                    print("Downloading file!!")
-                    OAuth_helpers.download_drive_file(creds=token, file_id=material.id, file_name=material.file_name)
-                    material.file = f"./course_material/{material.file_name}"
-                    material.save()
-                except:
-                    print("File is Not DOWNLOADABLE!!!")
-                    pass
+            for file in entry['materials']:
+                print("FILE :- ", file)
+                print("FILE ID :- ", file['driveFile']['driveFile']['id'])
+                material = Material.objects.filter(id=file['driveFile']['driveFile']['id'])
+                if not material:
+                    try:
+                        if file['driveFile']['driveFile']['title'].split('.')[-1] == 'mp4':
+                            continue
+
+                        print("HERE!!")
+                        material = Material(id=file['driveFile']['driveFile']['id'],
+                                            parent_course=course_name,
+                                            title=entry['title'],
+                                            file_name=file['driveFile']['driveFile']['title'],
+                                            creation_date=entry['creationTime'])
+                        print("Downloading file!!")
+                        OAuth_helpers.download_drive_file(creds=token, file_id=material.id,
+                                                          file_name=material.file_name)
+                        material.file = f"./course_material/{material.file_name}"
+                        material.save()
+                    except:
+                        print("File is Not DOWNLOADABLE!!!")
+                        pass
 
 
 @shared_task

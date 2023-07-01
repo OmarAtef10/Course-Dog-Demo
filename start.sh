ca013b2186ab3ad17feff4015ce7ada866a27791
Omarkhaledm21 <omarkhaledm21@gmail.com>
2023-07-01 08:17:20 +0300

    update all is working now

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-apt install python3-pip
-pip install watchdog celery
-watchmedo auto-restart --directory=. --pattern='*.py' --recursive -- celery -A ServerDemo worker -l info
+cd /app/
+watchmedo auto-restart --directory=. --pattern='*.py' --recursive -- celery -A ServerDemo worker -l info &
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000

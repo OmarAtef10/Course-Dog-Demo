f24c29f9a62806797808caef9c934fecfbeeceb3
Omarkhaledm21 <omarkhaledm21@gmail.com>
2023-07-01 21:40:18 +0300

    cron jobs added

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@ cd /app/
 watchmedo auto-restart --directory=. --pattern='*.py' --recursive -- celery -A ServerDemo worker -l info &
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 &
+sleep 20
+python manage.py runcrons

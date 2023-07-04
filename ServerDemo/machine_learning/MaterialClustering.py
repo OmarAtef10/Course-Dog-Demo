2881b3a513d715546d0fa1bc21239cfa5e5d5319
Peter Atef <peter.atef@coformatique.com>
2023-07-04 10:38:07 +0300

    fixed material cluster

--- ServerDemo/machine_learning/MaterialClustering.py
+++ ServerDemo/machine_learning/MaterialClustering.py
@@ -1,7 +1,7 @@
 from .Cluster import Cluster
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 class UKMeansClusterer:
     def vectorize(self, cluster1: Cluster, cluster2: Cluster):
@@ -23,7 +23,9 @@ class UKMeansClusterer:
         )[0][0]
     
     def get_cosine_similarity(self, cluster1: Cluster, cluster2: Cluster):
-        vectors = self.vectorize(cluster1, cluster2)
+        try: vectors = self.vectorize(cluster1, cluster2)
+        except: return 0
+
         return self.calculate_similairty(vectors)
     
 
@@ -37,8 +39,12 @@ class UKMeansClusterer:
         for cluster1_id, cluster1 in clusters.items():
             for cluster2_id, cluster2 in clusters.items():
                 if sim_matrix[cluster1_id][cluster2_id] != -1: continue
-                if(cluster1_id == cluster2_id): continue #dont self merge
-                if(not cluster1.isdisjoint(cluster2)): continue #don't merge clusters from same source
+                if(cluster1_id == cluster2_id): 
+                    sim_matrix[cluster1_id][cluster2_id] = 0
+                    continue #dont self merge
+                if(not cluster1.isdisjoint(cluster2)): 
+                    sim_matrix[cluster1_id][cluster2_id] = 0
+                    continue #don't merge clusters from same source
 
                 cosine_sim = self.get_cosine_similarity(cluster1, cluster2)
                 sim_matrix[cluster1_id][cluster2_id] = cosine_sim
@@ -50,7 +56,7 @@ class UKMeansClusterer:
 
     def get_most_similar_cluster(self, cluster1_Id, sim_matrix):
         max_sim = 0
-        max_cluster = cluster1_Id
+        max_cluster = None
         for cluster2_id, sim in sim_matrix[cluster1_Id].items():
             if sim > max_sim:
                 max_sim = sim
@@ -61,30 +67,46 @@ class UKMeansClusterer:
 
     def init_sim_matrix(self, clusters):
         return {
-            cluster1: {
-                cluster2: -1 for cluster2 in list(clusters.keys())
-            } for cluster1 in list(clusters.keys())
+            cluster1.clusterId: {
+                cluster2.clusterId: -1 for cluster2 in clusters.values()
+            } for cluster1 in clusters.values()
         }
     
-
+    def update_keys(self, clusters):
+        return {cluster.clusterId: cluster for cluster in clusters.values()}
     def migrate_sim_matrix(self, clusters: dict, sim_matrix: dict):
         new_sim_matrix = self.init_sim_matrix(clusters)
 
-        for cluster1_id, cluster1 in clusters.items():
-            for cluster2_id, cluster2 in clusters.items():
-                cosine_sim = sim_matrix.get(cluster1_id).get(cluster2_id)
-                if cosine_sim == None:
+        for _, cluster1 in clusters.items():
+            for _, cluster2 in clusters.items():
+                cluster1_id, cluster2_id = cluster1.clusterId, cluster2.clusterId
+
+                if cluster1_id == cluster2_id: 
+                    new_sim_matrix[cluster1_id][cluster2_id] = 0
+                    continue
+
+                if(not cluster1.isdisjoint(cluster2)): 
+                    new_sim_matrix[cluster1_id][cluster2_id] = 0
+                    continue #don't merge clusters from same source
+
+                row = sim_matrix.get(cluster1_id, None)
+                if row == None:
                     cosine_sim = self.get_cosine_similarity(cluster1, cluster2)
+                else:   
+                    cosine_sim = row.get(cluster2_id, None)
+                    if cosine_sim == None:
+                        cosine_sim = self.get_cosine_similarity(cluster1, cluster2)
 
                 new_sim_matrix[cluster1_id][cluster2_id] = cosine_sim
                 new_sim_matrix[cluster2_id][cluster1_id] = cosine_sim
 
-        return new_sim_matrix
+        clusters = self.update_keys(clusters)
+        return new_sim_matrix, clusters
     
 
     def clean_clusters(self, clusters):
         return {
-            cluster_id: cluster for cluster_id, cluster in clusters.items() if cluster != None
+            clusterId: cluster for clusterId, cluster in clusters.items() if cluster != None
         }
 
     def extract_output(self, clusters):
@@ -113,12 +135,15 @@ class UKMeansClusterer:
             if not sim_matrix:
                 sim_matrix = self.create_sim_matrix(clusters)
             else:
-                sim_matrix = self.migrate_sim_matrix(clusters, sim_matrix)
+                sim_matrix, clusters = self.migrate_sim_matrix(clusters, sim_matrix)
 
             for clusterId, cluster in clusters.items():
                 most_similar_to, score = self.get_most_similar_cluster(clusterId, sim_matrix)
-
-                if(self.should_merge(score)):
+                
+                if(self.should_merge(score) 
+                    and most_similar_to != None 
+                    and clusters[most_similar_to].clusterId != clusterId
+                ):
                     cluster.merge_with_cluster(clusters[most_similar_to])
                     clusters[most_similar_to] = None
                     merged_cluster = True
@@ -126,4 +151,6 @@ class UKMeansClusterer:
 
             clusters = self.clean_clusters(clusters)
 
-        return self.extract_output(clusters)
+        output = self.extract_output(clusters)
+        for c in output: print(c)
+        return output
